ccd26b48df270cd60cda3e3c321d8b63642a6f8b
Chirag Sharma <Chiragsharma250999@gmail.com>
2023-02-08 01:13:15 +0530

    Added css & created/passed variables to components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,16 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return (<div>
-        <div>Date</div>
-        <div><h2>Title</h2></div>
-        <div>Amount</div>
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.7;
+    const LocationOfExpenditure = 'Mumbai';
+
+    return (<div className='expense-item'>
+        <div>{expenseDate.toISOString()}</div>
+        <div className='expense-item__description'><h2>{expenseTitle}</h2></div>
+        <div className='expense-item__description'><h2>{LocationOfExpenditure}</h2></div>
+        <div className='expense-item__price'>${expenseAmount}</div>
     </div >)
 };
 
