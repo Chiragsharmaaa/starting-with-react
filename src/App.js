d4c0709e0c79899d2531f5181902b9b81f042285
Chirag Sharma <Chiragsharma250999@gmail.com>
2023-02-16 18:17:37 +0530

    created form!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from "./components/Expenses/Expenses";
 import './App.css';
 
 const App = () => {
   return (
     <div className="app-comp-class">
+      <NewExpense />
       <Expenses />
     </div>
   )
